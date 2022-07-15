55efe2d4b9eca5d353cbc62eae7fbbff1733c491
caasib <107368103+caasib@users.noreply.github.com>
2022-07-15 14:32:46 -0400

    edited storing fingerprints

--- CogZam/store_fingersprints.py
+++ CogZam/store_fingersprints.py
@@ -1,4 +1,6 @@
-def store_fingerprint(fanout, song_ID):
+def store_fingerprint(fanout, song_ID, song_database, fp_database):
+    if song_ID in song_database:
+        return "That song ID is already in use"
     for (fm, fn, dt), tm in fanout:
         fp_database[(fm, fn, dt)].append((song_ID, tm))
     with open("placeholder", mode="wb") as f:
