30452e643f2e3ccc79db564f20810d9dbef6069e
AWc31s <awangc31s@gmail.com>
2022-07-15 14:38:59 -0400

    edited init

--- CogZam/__init__.py
+++ CogZam/__init__.py
@@ -2,3 +2,4 @@ from .getFingerprints import getFingerprints
 from .long_to_short import long_to_short
 from .peakExtraction import peak_extract
 from .recordingtoarray import userinput
+from .store_fingerprints import store_fingerprint
