82c6879627dbe46f727d37ce4cc6a0ff3b8f3df7
AWc31s <awangc31s@gmail.com>
2022-07-15 14:49:11 -0400

    imported database.py

--- CogZam/__init__.py
+++ CogZam/__init__.py
@@ -3,3 +3,4 @@ from .long_to_short import long_to_short
 from .peakExtraction import peak_extract
 from .recordingtoarray import userinput
 from .store_fingerprints import store_fingerprint
+from .database_interface import store_song
