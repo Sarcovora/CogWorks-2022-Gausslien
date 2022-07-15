c0c14fc7e854dc323c382bf172259e1d1513eb93
caasib <107368103+caasib@users.noreply.github.com>
2022-07-15 14:42:27 -0400

    removed placeholder from store_fingerprints

--- CogZam/store_fingersprints.py
+++ CogZam/store_fingersprints.py
@@ -3,5 +3,5 @@ def store_fingerprint(fanout, song_ID, song_database, fp_database):
         return "That song ID is already in use"
     for (fm, fn, dt), tm in fanout:
         fp_database[(fm, fn, dt)].append((song_ID, tm))
-    with open("placeholder", mode="wb") as f:
+    with open("fingerprint_database.pkl", mode="wb") as f:
         pickle.dump(fp_database, f)
